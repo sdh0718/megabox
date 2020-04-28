6f7bbf7743430da560827dcc214fcbea1f23138e
암바사 <64447906+sdh0718@users.noreply.github.com>
2020-04-28 15:00:12 +0900

    Add files via upload

---
+++ js/banner.js
@@ -0,0 +1,14 @@
+    $(function(){
+        // close banner
+        $('.close').click(function(){
+            $('.banner>div').hide();
+        });
+        //random banner
+        $('.banner>div').removeClass('active');
+        var ranNum=Math.random();
+        if(ranNum>=0.5){
+            $('.banner02').addClass('active');
+        }else{
+            $('.banner01').addClass('active');
+        }
+    });
